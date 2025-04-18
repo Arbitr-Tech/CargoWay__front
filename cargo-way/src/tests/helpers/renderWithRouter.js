7aabe8e15b18b23aa0de715ea0c62ed3da4ef0bd
TenAnnaTen <ann.ten.tkdlove@mail.ru>
2025-04-18 22:48:04 +0300

    #48902489. Написание автоматических тестов

--- cargo-way/src/tests/helpers/renderWithRouter.js
+++ cargo-way/src/tests/helpers/renderWithRouter.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-
-
 import AppRouter from "../../routes/AppRouter"
 
 export const renderWithRouter = (component, initialRoute = '/') => {
